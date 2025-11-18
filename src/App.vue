fd8320f45061ee02135b17306e0568615d512320
Andrei Babkin <bandit.tcpc@gmail.com>
2025-11-18 11:39:49 +0300

    add calendar to main app component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>Initial</div>
+    <div id="app">
+        <CalendarMain :sharedDate="sharedDate" />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CalendarMain from '@/components/Calendar/CalendartMain.vue';
+import { ref } from 'vue';
+
+const sharedDate = ref(null);
+</script>
 
 <style>
 body {
-  font-family: Helvetica, sans-serif;
+    font-family: Helvetica, sans-serif;
+}
+
+* {
+    box-sizing: border-box;
 }
 </style>
